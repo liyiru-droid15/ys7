122ab79ae72d50a874b43c3c70610127bc3b5d47
liyiru <droid15@126.com>
2018-12-24 11:09:26 +0800

    缓存优化

--- src/Cache.php
+++ src/Cache.php
@@ -1,40 +1,86 @@
 <?php 
 
 namespace droid15;
-class Cache{
-	static $cachePath;
-	public function __construct(){
-		self::$cachePath = 'cache/';
-	}
 
-	public static function set($k,$value)
+//默认缓存目录，建议不修改以免删除重要数据
+define("CACHE_PATH", __DIR__.'/cache/');
+
+class Cache {	
+	//static $cachePath = __DIR__.'/cache/';
+	/**
+	 * 存缓存
+	 * @param [string] $k     [缓存键名]
+	 * @param [mix] $value [缓存内容]
+	 */
+	public static function set($k, $value)
 	{
+
 		if(!is_string($k) || !trim($k)){
 			return false;
 		}
-		$key = self::$cachePath.sha1(md5($k.date('Ymd')));
+		$key = CACHE_PATH.sha1(md5($k.date('Ymd')));
+
+		if(!is_dir(CACHE_PATH)){
+			mkdir(CACHE_PATH);
+		}
+
 		if(is_object($value)){
 			$value = serialize($value);
 		}
+
 		$res = file_put_contents($key, $value,LOCK_EX);
 		return $res;
 	}
 
-	public static function get($k)
+	/**
+	 * 取缓存
+	 * @param  [type]  $key     [缓存名]
+	 * @param  boolean $distory [是否清理过期缓存]
+	 * @return [type]           [description]
+	 */
+	public static function get($key, $distory=true)
 	{
-		if(!is_string($k) || !trim($k)){
+		if(!is_string($key) || !trim($key)){
 			return false;
 		}
-		$oldCache = self::$cachePath.sha1(md5( $k.date('Ymd',strtotime('-1 day')) ));
-		if(file_exists($oldCache)){
-			unlink($oldCache);
-		}
-		$key = self::$cachePath.sha1(md5($k.date('Ymd')));
+		//取当天的缓存
+		$name = sha1(md5($key.date('Ymd')));
+		$key = CACHE_PATH.$name;
+
 		if(!file_exists($key)){
 			return false;
 		}
 
+		if($distory){
+			$files = scandir(CACHE_PATH);
+			foreach ($files as $k => $v) {
+				if($v!='.' && $v!='..' && $v!=$name){
+				unlink(CACHE_PATH.$v);
+				}
+			}	
+		}
+
 		$res = file_get_contents($key);
 		return $res;
+	}
+
+	/**
+	 * 清理缓存目录下所有缓存
+	 * @return [type] [description]
+	 */
+	public static function clearAll()
+	{
+		if(!is_dir(CACHE_PATH)){
+			return false;
+		}
+
+		$files = scandir(CACHE_PATH);
+		foreach ($files as $k => $v) {
+			if($v!='.' && $v!='..'){
+				unlink(CACHE_PATH.$v);
+			}
+		}	
+		
+		return true;		
 	}
 }
--- test.php
+++ test.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use droid15\Run;
 
-$ys7 = new Run('your appKey','your appSecret');
+$ys7 = new Run('Your appKey','Your appSecret');
 
 var_dump($ys7->deviceList());
+//var_dump($ys7->getPlayUrl(['sn'=>'A123456']));
